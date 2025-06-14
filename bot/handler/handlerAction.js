b41627bb895d9e3b148a2e7e2ca0be7c0eb39109
ᏦᎽᎾᎿᎯᏦᎯ <stromjersey@gmail.com>
2025-06-14 14:53:27 +0100

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -45,7 +45,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				onReaction();
 				if (event.reaction == "👎") {
     if (
-        event.userID === "100068909067279"
+        event.userID === "61577043882265"
     ) {
         api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
             if (err) return console.log(err);
@@ -56,7 +56,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 if (event.reaction == "😠") {
     if (event.senderID == api.getCurrentUserID()) {
         if (
-            event.userID === "100068909067279"
+            event.userID === "61577043882265"
         ) {
             api.unsendMessage(event.messageID, (err) => {
                 if (err) return console.log(err);
